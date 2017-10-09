a36a6e802c0a43a69fef191baac63f02e71f93f3
Bruno Jouhier <bjouhier@gmail.com>
2017-10-09 18:56:36 +0200

    another ts api fix

--- index.ts
+++ index.ts
@@ -3,4 +3,4 @@ import * as oids from './src/oids';
 import * as x509 from './src/x509';
 
 export { asn1, oids, x509 };
-export { stripEncryption, Certificate, integrity } from './src/certificate';
+export * from './src/certificate';
